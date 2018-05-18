1b9859acd4e2dd64184fb9675d5b07bea122d854
Rikard Olsson <rikard@thryselius.se>
2018-05-18 18:56:11 +0200

    Before changing everything

--- hw5.py
+++ hw5.py
@@ -90,116 +90,147 @@ def k_means(X, K):
     # At this point, the mu's are "optimal" (could be local optimum)
     return mu, z, clusters
 
-mu, z, clusters = k_means(X,2)
+# mu, z, clusters = k_means(X,2)
 
-# Plot when 2 iterations
-fig = plt.figure(figsize=(10,10))
-ax1 = fig.add_subplot(221)
-ax1.set_title("B) After 2 iterations")
-colors_1 = []
-for i in range(0, len(clusters[0][0])):
-    if clusters[0][0][i] == 1:
-        colors_1.append("r")
-    else:
-        colors_1.append("b")
-ax1.scatter(X[:,0],X[:,1],color=colors_1)
+# # Plot when 2 iterations
+# fig = plt.figure(figsize=(10,10))
+# ax1 = fig.add_subplot(221)
+# ax1.set_title("B) After 2 iterations")
+# colors_1 = []
+# for i in range(0, len(clusters[0][0])):
+#     if clusters[0][0][i] == 1:
+#         colors_1.append("r")
+#     else:
+#         colors_1.append("b")
+# ax1.scatter(X[:,0],X[:,1],color=colors_1)
 
-# Plot when convergence reached
-ax2 = fig.add_subplot(222)
-ax2.set_title("B) Final")
-colors_2 = []
-for i in range(0, len(clusters[1][0])):
-    if clusters[1][0][i] == 1:
-        colors_2.append("r")
-    else:
-        colors_2.append("b")
-ax2.scatter(X[:,0],X[:,1],color=colors_2)
+# # Plot when convergence reached
+# ax2 = fig.add_subplot(222)
+# ax2.set_title("B) Final")
+# colors_2 = []
+# for i in range(0, len(clusters[1][0])):
+#     if clusters[1][0][i] == 1:
+#         colors_2.append("r")
+#     else:
+#         colors_2.append("b")
+# ax2.scatter(X[:,0],X[:,1],color=colors_2)
 
 ################
 # Show 
-plt.show()
+#plt.show()
 
 ################################################## c) #############################################
 mat = scipy.io.loadmat('hw5_p1b.mat')
 Y = mat['X']
 
-def kernal_k_means(X, K, sigma):
+class Kernal_k_means:
 
-    # Data length
-    length = len(X)
-    
-    # RBF Kernal
-    def rbf(x1, x2, sigma):
-        delta = abs(np.subtract(x1, x2))
-        squaredEuclidean = np.square(delta)
-        result = np.exp(-(squaredEuclidean)/(2*sigma**2))
-        return result
+    def __init__(self, data, K, sigma):
+        self.X = data
+        self.K = K
+        self.sigma = sigma
+        self.length = len(X)
 
-    # Second term of the distance function defined in lecture notes
-    def snd_term(x, Nk, X, z, k):
+        self.z = Kernal_k_means.initZ(K)
+
+    def mu(self, x, k, Nk):
         r = 0
         l = len(z[k])
         for m in range(0, l):
-            r += z[k][m]*rbf(x, X[m])
+            r += self.z[k][m]*self.rbf(x, self.X[m])
 
-        return (2*r) / Nk
+        return r / Nk
+    
+    # RBF Kernal
+    def rbf(self, x1, x2):
+        delta = np.linalg.norm(x1-x2)
+        se = np.square(delta)
+        result = np.exp(-(se)/(2*self.sigma**2))
+        return result
+
+    # Second term of the distance function defined in lecture notes
+    def snd_term(self, x, Nk, k):
+        return 2*self.mu(x,k,Nk)
 
     # Third term of the distance function defined in lecture notes
-    def td_term(Nk, X, z, k):
+    def td_term(self, Nk, k):
         r = 0
         lt = len(z[k])
         for m in range(0, lt):
-            for l in range(0, l):
-                r += z[k][m]*z[k][l]*rbf(X[m], X[l])
+            for l in range(0, lt):
+                r += self.z[k][m]*self.z[k][l]*self.rbf(self.X[m], self.X[l])
 
         return r / (2*Nk)
 
     # Sum of number of points assigned to the k'th z
-    def Nk(z,k):
-        return sum(z[k])
+    def Nk(self,k):
+        return sum(self.z[k])
 
     # Create's K number of dictionaries to map the points
+    @staticmethod
     def initZ(K):
         # Define z as the centriods for which the points are assigned to
-        z = {}
+        z = []
         for k in range(0,K):
-            z[k] = {}
+            z.append([])
             # Assign random points to z_nk
             for n in range(0,length):
                 if np.random.uniform() > 0.5:
-                    z[k][n] = 1
+                    z[k].append(1)
                 else:
-                    z[k][n] = 0
+                    z[k].append(0)
         return z
 
     # Distance function as defined in lecture notes
-    def distance(x, Nk, data, z, k, sigma):
-        return rbf(x, x, sigma) - snd_term(x, Nk, data, z, k) + td_term(Nk, data, z, k)
+    def distance(self, x, Nk, k):
+        return 1 - self.snd_term(x, Nk, k) + self.td_term(Nk, k)
+
+    # Computed kernals. (x_n, x_m): 1 or 0
+    kernals = {}
 
     # Create the z's
-    z = initZ(K)
+    z = np.asmatrix(initZ(K))
 
-    while (True):
+    # while (True):
 
-        # Create a deepcopy of latest z
-        z_n = copy.deepcopy(z)
+    #     # Create a deepcopy of latest z
+    #     z_n = copy.deepcopy(z)
 
-        # For each point in X, assign it to the center with nereast distance
-        for i in len(X):
-            x = X[i]
-            nearest = { "k": -1, "dis": 99 }
-            for k in range(0,K):
-                d = distance(x, Nk(z,k))
-                if d < nearest["dis"]:
-                    nearest["k"] = k
+    #     # For each point in X, assign it to the center with nereast distance
+    #     for i in range(0,length):
+    #         x = X[i]
+    #         nearest = { "k": -1, "dis": 99 }
+    #         for k in range(0,K):
+    #             d = distance(x, Nk(z,k), X, z, k, sigma)
+    #             if d < nearest["dis"]:
+    #                 nearest["k"] = k
+    #                 nearest["dis"] = d
             
-            # Assign 1 to nearest, 0 to others
-            z[nearest["k"]][i] = 1
-            for k_n in range(0, K):
-                if not k_n == nearest["k"]:
-                    z[k_n][i] = 0
+    #         # Assign 1 to nearest, 0 to others
+    #         z[nearest["k"]][i] = 1
+    #         for k_n in range(0, K):
+    #             if not k_n == nearest["k"]:
+    #                 z[k_n][i] = 0
 
-        # Check if z is 
-        if np.array_equal(z, z_n):
-            break
+    #     # Check if z is 
+    #     if np.array_equal(z, z_n):
+    #         break
+    print(z)
+    
+    # Return z
+    return z
 
+z = kernal_k_means(Y,2,0.2)
+
+# # Plot when convergence reached
+# ax3 = fig.add_subplot(223)
+# ax3.set_title("C)")
+# colors = []
+# for i in range(0, len(z[1])):
+#     if z[1][i] == 1:
+#         colors.append("r")
+#     else:
+#         colors.append("b")
+# ax3.scatter(Y[:, 0], Y[:, 1], color=colors)
+
+# plt.show()
